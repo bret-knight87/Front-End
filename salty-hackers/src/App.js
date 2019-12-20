7478640b2fc1980bd8995a5b4ad989eae2a914cc
bret-knight87 <bret.knight87@gmail.com>
2019-12-19 22:23:23 -0600

    fixed security issue - serialize-javascript

--- salty-hackers/src/App.js
+++ salty-hackers/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import SignIn from './components /SignIn';
 
 function App() {
+  
   return (
     <div className="App">
       <h1>Need salt with that?</h1>
+      <SignIn />    
     </div>
   );
 }
---
+++ salty-hackers/src/components /SignIn.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+import CreateAccount from './CreateAccount';
+
+const SignIn = () => {
+    document.title = 'Sign In | SHNT'
+
+    return (
+        <div className="App">
+            
+        </div>
+    );
+}
+
+export default SignIn;
