8474bb5fe489ce63af5b5ff2c2ed4e670ab3ce50
bret-knight87 <bret.knight87@gmail.com>
2020-01-06 21:42:24 -0600

    hard coded nav bar and sign in page

--- salty-hackers/src/App.js
+++ salty-hackers/src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components"
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import SignIn from './components /SignIn';
+
+
+const Nav = styled.nav`
+  border: 1px solid black;
+  display: flex;
+`;
+
+const Title = styled.h1`
+  color: #2A9D8F;
+  font-size: 4rem;
+  border: solid;
+  margin-left: 3%;
+  margin-top: 3%;
+`;
 
 function App() {
   
   return (
-    <div className="App">
-      <h1>Need salt with that?</h1>
-      <SignIn />    
+    <div className='App'>
+      <Nav>
+        <img src='/imgs/trollmain.jpg'/>
+      <Title>HN Trolls!</Title>
+      </Nav>
+      <SignIn />  
     </div>
   );
 }
--- salty-hackers/src/components /SignIn.js
+++ salty-hackers/src/components /SignIn.js
@@ -1,18 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { withFormik, Form, Field } from 'formik';
-import * as Yup from 'yup';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from 'axios';
+import CreateAccount from "./CreateAccount";
 
-import CreateAccount from './CreateAccount';
+const SignInForm = ({ values, errors, touched, status }) => {
+  document.title = "Sign In | SHNT";
 
-const SignIn = () => {
-    document.title = 'Sign In | SHNT'
+  const [signIn, setSignIn] = useState([]);
 
-    return (
-        <div className="App">
-            
-        </div>
-    );
-}
+  return (
+    <div className="App">
+      <Form>
+        <label htmlFor="email">
+          E-Mail:
+          <Field id="email" type="text" name="email" />
+          {touched.email && errors.email && <p>{errors.email}</p>}
+        </label>
+        <label htmlFor="password">
+            Password:
+            <Field id="password" type="password" name="password" />
+  {touched.email && errors.password && <p>{errors.password}</p>}
+        </label>
+      </Form>
+    </div>
+  );
+};
 
-export default SignIn;
+const FormikSignInForm = withFormik({
+  mapPropsToValues(props) {
+    return {
+      email: props.email || "",
+      password: props.password || "",
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+      email: Yup.string().required(),
+      password: Yup.string().required()
+  }),
+
+  handleSubmit(values, { setStatus, resetForm }) {
+      axios
+      .post("https://reqres.in/api/users/", values)
+      .then (res => {
+          console.log("success", res);
+
+          resetForm();
+      })
+      .catch(err => console.log(err.response));
+  }
+})(SignInForm);
+
+export default FormikSignInForm;
