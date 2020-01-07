71562545bc844e47448e0500e967c5f3512524cd
bret-knight87 <bret.knight87@gmail.com>
2020-01-06 23:06:04 -0600

    changed font

--- salty-hackers/src/App.js
+++ salty-hackers/src/App.js
@@ -2,27 +2,36 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import SignIn from "./components /SignIn";
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Shadows+Into+Light&display=swap');
+  body {
+    font-family: 'Notable', sans-serif;
+  }
+`;
 
 const Nav = styled.nav`
   // border: 1px solid black;
   display: flex;
-  background-color: #E9C46A
+  background-color: #e9c46a;
 `;
 
 const Title = styled.h1`
   color: #2a9d8f;
-  font-size: 4rem;
+  font-size: 5rem;
   // border: solid;
   margin-left: 3%;
   margin-top: 3%;
+  font-family: 'Shadows Into Light', cursive;
 `;
 
 const Footer = styled.footer`
   // border: 1px solid black;
   display: flex;
-  background-color: #E9C46A
+  background-color: #e9c46a;
 `;
 
 const FooterNav = styled.nav`
@@ -32,25 +41,37 @@ const FooterNav = styled.nav`
   justify-content: space-between;
   align-items: center;
   width: 15%;
-  
 `;
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Nav>
         <img src="/imgs/trollmain.jpg" />
-        <Title>S/HN Trolls!</Title>
+        <Title>SHN Trolls!</Title>
       </Nav>
       <SignIn />
       <Footer>
         <p className="copy-right">&copy; 2020 SHNT!</p>
         <FooterNav>
-          <p><a className="Foot-nav" href="#home">Home</a></p>
+          <p>
+            <a className="Foot-nav" href="#home">
+              Home
+            </a>
+          </p>
           <p>|</p>
-          <p><a className="Foot-nav" href="#about">About</a></p>
+          <p>
+            <a className="Foot-nav" href="#about">
+              About
+            </a>
+          </p>
           <p>|</p>
-          <p><a className="Foot-nav" href="#contact">Contact</a></p>
+          <p>
+            <a className="Foot-nav" href="#contact">
+              Contact
+            </a>
+          </p>
         </FooterNav>
       </Footer>
     </div>
